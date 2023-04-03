fda1ed87dac13755e3b7ac7257ed053d3e3bf35f
ggoulartm <ggoulartm@gmail.com>
2023-04-03 19:15:20 -0300

    ready

--- analysis.py
+++ analysis.py
@@ -10,20 +10,21 @@ cont = {}
 
 for a in ['A','T','C','G']:
 	for b in ['A','T','C','G']:
-		cont[a+b]=0
+		cont[a+b] = 0
 
-entrada.replace("\n","")
+entrada = entrada[77:]
+entrada = entrada.replace("\n","")
 
-for par in range(len(entrada)-1):
-	cont[entrada[par]+entrada[par+1]]+=1
+for k in range(len(entrada)-1):
+	aux=entrada[k]+entrada[k+1]
+	cont[aux] += 1
 
 #HTML
 
 i = 1
-
 for result in cont:
-	transp = cont[k]/max(cont.values())
-	saida.write("<div style='width:100px; border:1px solid; height:100px; float:left; background-color:rgba(0,0,0,"+str(transp)+")'>+str(k)+</div>")
+	transp = cont[result]/max(cont.values())
+	saida.write("<div style='width:100px; border:1px solid; height:100px; float:left; background-color:rgba(0,0,0,"+str(transp)+")'>"+str(result)+"</div>")
 	if i%4==0:
 		saida.write("<div style='clear:both'></div>")
 	i+=1
