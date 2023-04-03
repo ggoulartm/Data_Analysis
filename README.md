4eb4c883e3a5717e1e7c2fd4429b87fdd159891e
ggoulartm <ggoulartm@gmail.com>
2023-04-03 19:24:23 -0300

    html

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 The main topic of this repository is to study data analysis with python
 
 First of all, I'll bring some data plots
+
+![Bacteria Genoma](bacteria.html)>>README.md
+
+![Human Genoma](human.html)>>README.md
--- analysis.py
+++ analysis.py
@@ -24,7 +24,35 @@ for k in range(len(entrada)-1):
 i = 1
 for result in cont:
 	transp = cont[result]/max(cont.values())
-	saida.write("<div style='width:100px; border:1px solid; height:100px; float:left; background-color:rgba(0,0,0,"+str(transp)+")'>"+str(result)+"</div>")
+	saida.write("<div style='width:100px; border:1px solid; height:25px; color:#fff; float:left; background-color:rgba(0,0,0,"+str(transp)+")'><center>"+str(result)+":"+str(cont[result])+"</center></div>")
+	if i%4==0:
+		saida.write("<div style='clear:both'></div>")
+	i+=1
+saida.close()
+
+entrada = open("human.fasta").read()
+saida = open("human.html","w")
+
+
+cont = {}
+
+for a in ['A','T','C','G']:
+	for b in ['A','T','C','G']:
+		cont[a+b] = 0
+
+entrada = entrada[38:]
+entrada = entrada.replace("\n","")
+
+for k in range(len(entrada)-1):
+	aux=entrada[k]+entrada[k+1]
+	cont[aux] += 1
+
+#HTML
+
+i = 1
+for result in cont:
+	transp = cont[result]/max(cont.values())
+	saida.write("<div style='width:100px; border:1px solid; height:25px; color:#fff; float:left; background-color:rgba(0,0,0,"+str(transp)+")'><center>"+str(result)+":"+str(cont[result])+"</center></div>")
 	if i%4==0:
 		saida.write("<div style='clear:both'></div>")
 	i+=1
